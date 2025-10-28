c0a84eb5163cd631d3272c65014ef4ebd61d01d6
ravisankar666 <ravisankarp666@gmail.com>
2025-10-29 01:17:59 +0530

    some message

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "main" {
-  name        = var.sg_name #catalogue
+  name        = var.sg_name # catalogue
   description = var.sg_description
   vpc_id      = var.vpc_id
 
@@ -8,16 +8,13 @@ resource "aws_security_group" "main" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    
   }
-
-
 
   tags = merge(
     var.sg_tags,
     local.common_tags,
     {
-        Name = "${local.common_name_suffix}-${var.sg_name}" #roboshop-dev catalogue
+        Name = "${local.common_name_suffix}-${var.sg_name}" # roboshop-dev-catalogue
     }
   )
 }
