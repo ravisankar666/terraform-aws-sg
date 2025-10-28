8fee733b672eb2fbb9bf1d7c7b291bb219cd57ed
ravisankar666 <ravisankarp666@gmail.com>
2025-10-29 01:14:13 +0530

    some message

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "main" {
   name        = var.sg_name #catalogue
   description = var.sg_description
-  vpc_id      = var.vpc_id
+  vpc_id      = var.vpc_id["id"]
 
   egress {
     from_port        = 0
